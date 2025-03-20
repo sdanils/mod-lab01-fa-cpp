f4a81e4dcc53d4758f58fced33eeaaf864f6dabd
Daniil Salomasov <danil.salomasov.np@gmail.com>
2025-03-20 23:45:32 +0300

    + Реализация функций + main

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,68 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+  int count = 0;
+  bool inWord = false;
+  while (*str) {
+    if (*str != ' ' && inWord == false) {
+      count++;
+      inWord = true;
+    } else if (*str == ' ' && inWord == true) {
+      inWord = false;
+    }
+    str++;
+  }
+  return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+  int count = 0;
+  bool inWord = false;
+  while (*str) {
+    if (*str >= 65 && *str <= 90) {
+      count++;
+      inWord = true;
+      str++;
+      bool correct = true;
+      while (*str) {
+        if (*str == ' ') {
+          inWord = false;
+          break;
+        } else if (*str < 97 || *str > 122 && correct == true) {
+          count--;
+          correct = false;
+        }
+        str++;
+      }
+    } else if (*str == ' ' && inWord == true) {
+      inWord = false;
+    }
+    str++;
+  }
+  return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+  int count = 0, num_chars = 0;
+  bool inWord = false;
+  while (*str) {
+    num_chars++;
+    if (*str != ' ' && inWord == false) {
+      count++;
+      inWord = true;
+    } else if (*str == ' ') {
+      num_chars--;
+      if (inWord == true) inWord = false;
+    }
+    str++;
+  }
+  double result = num_chars / count;
+  result *= 10;
+  int result_int = result;
+  if (result_int % 10 >= 5)
+    result_int = result_int / 10 + 1;
+  else
+    result_int = result_int / 10;
+
+  return result_int;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "fun.h"
 
+using namespace std;
+
 int main() {
-    return 0;
+  vector<string> strings = {"qwe4  gftt e 555gr rrgr  grhh ",
+                            "  qwe4 ",
+                            "Hello, world!",
+                            "Hello world!",
+                            " bad dog ",
+                            " six monkeys"};
+
+  if (faStr1(strings[0].c_str()) != 6) {
+    cout << " Uncor 0";
+  }
+  if (faStr1(strings[1].c_str()) != 1) {
+    cout << " Uncor 1";
+  }
+  if (faStr2(strings[2].c_str()) != 0) {
+    cout << " Uncor 2";
+  }
+  if (faStr2(strings[3].c_str()) != 1) {
+    cout << " Uncor 3";
+  }
+  if (faStr3(strings[4].c_str()) != 3) {
+    cout << " Uncor 4";
+  }
+  if (faStr3(strings[5].c_str()) != 5) {
+    cout << " Uncor 5";
+  }
+
+  return 0;
 }
